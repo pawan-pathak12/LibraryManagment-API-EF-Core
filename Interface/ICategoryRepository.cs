94988b73f3c261b9c097cde4a9468bc1552fc534
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-19 18:29:16 +0545

    Added Interface and Repository code of Cateogry

---
+++ Interface/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library_Management_API.Models;
+
+namespace Library_Management_API.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task<int> AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
